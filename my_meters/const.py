c71c23c14fd6931b3e328381bae5ad720b0969f6
toha17171 <104164561+toha17171@users.noreply.github.com>
2026-02-25 23:25:55 +0300

    Delete my_meters directory

--- my_meters/const.py
+++
@@ -1,6 +0,0 @@
-"""Constants for the My Meters integration."""
-
-DOMAIN = "my_meters"
-CONF_API_URL = "api_url"
-CONF_TOKEN = "api_token"
-DEFAULT_SCAN_INTERVAL = 3600  # Обновление раз в час (в секундах)
